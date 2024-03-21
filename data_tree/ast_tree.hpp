81a51fdce08cbf4bc1f746d8cdc8cd568018bb80
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-21 14:34:14 +0300

    attempt to resolve leakage problem, but smth else neads to be done

--- data_tree/ast_tree.hpp
+++ data_tree/ast_tree.hpp
@@ -2,18 +2,23 @@
 
 #include "base_ast_node.hpp"
 #include "scope_node.hpp"
+#include "deque" //only insertion occurs
+
 
 namespace AST {
     class astree {
         scope_node* root = nullptr;
+        std::deque<base_ast_node*> node_storage;
 
     public:
         template <typename NodeType, typename... t_args> auto make_node(t_args && ... args){
             auto new_node = new NodeType(std::forward<t_args>(args)...);
+            node_storage.push_back(new_node);
             return new_node;
         }
 
         void set_root(scope_node* ptr) {root = ptr; }
         auto get_root() { return root; }
+        ~astree() = default;
     };
 }
