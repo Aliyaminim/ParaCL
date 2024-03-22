f6f6530f501b811b2303ba34ef6170421fa40c70
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-22 17:27:39 +0300

    restructured tests and now test invoking is much more prettier

--- test/testing.sh
+++ test/testing.sh
@@ -5,16 +5,21 @@ echo
 
 echo -e "\e[1mTesting well-formed paracl program\e[0m"
 echo
-for file in examples/*.in; do
+for file in test/well_formed/*.in; do
     echo $file
     valgrind ./build/ParaCL < $file
     echo
 done
 
+for file in test/*.in; do
+    echo $file
+    ./build/ParaCL < $file
+    echo
+done
 
 echo -e "\e[1mTesting ill-formed paracl program\e[0m"
 echo
-for file in examples/wrong/*.in; do
+for file in test/ill_formed/*.in; do
 echo $file
     ./build/ParaCL < $file
     echo
