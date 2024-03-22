bc4636a635f3cca2a504fd6e0b5130463b8a12b4
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-22 14:37:02 +0300

    new methods

--- data_tree/symbol_table.hpp
+++ data_tree/symbol_table.hpp
@@ -8,8 +8,12 @@ namespace AST {
 class Symbol_table final {
     std::unordered_map<std::string, VAL_TYPE> var_names;
 public:
+  bool contains(const std::string &name) const { return var_names.contains(name); }
+  void add(const std::string &name) { var_names.emplace(name, VAL_TYPE{}); }
 
   VAL_TYPE &operator[](const std::string &name) {   return var_names[name]; }
+
+  std::unordered_map<std::string, VAL_TYPE>* get_vars() { return &var_names; }
 
 };
 
