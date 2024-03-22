746d7451b392f27951850046e5ad9ee79b05cf39
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-22 14:35:17 +0300

    added curr_scope as attribute and useful methods to connect with Symbol table

--- data_tree/variable_expr.hpp
+++ data_tree/variable_expr.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
 #include "base_ast_node.hpp"
+#include "scope_node.hpp"
 #include <string>
 
 namespace AST {
     class variable_expr : public base_expr_node {
-        std::string name;
+        std::string name_;
         VAL_TYPE value;
+        scope_node* curr_scope;
     public:
-        explicit variable_expr(std::string nm) : base_expr_node(base_expr_node_type::VAR_EXPR), name(nm) {}
+        explicit variable_expr(std::string nm, scope_node* curr_sc_) : base_expr_node(base_expr_node_type::VAR_EXPR),
+            name_(nm), curr_scope(curr_sc_) {}
 
         VAL_TYPE get_value(){
             return value;
@@ -18,5 +21,12 @@ namespace AST {
             value = res;
             return;
         }
+
+        const std::string &name() const noexcept {
+            return name_;
+        }
+
+        scope_node* get_scope() { return curr_scope->find_var(name_); }
+
     };
 }
