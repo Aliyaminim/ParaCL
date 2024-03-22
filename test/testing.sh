3e707b7fcd458e47e00e35e8737ea6ff56ae0a9b
Minimullina Aliya <minimullina.ae@phystech.edu>
2024-03-22 22:03:14 +0300

    changes according to new processing input routine

--- test/testing.sh
+++ test/testing.sh
@@ -7,13 +7,13 @@ echo -e "\e[1mTesting well-formed paracl program\e[0m"
 echo
 for file in well_formed/*.in; do
     echo $file
-    ../build/ParaCL < $file
+    ../build/ParaCL $file
     echo
 done
 
 for file in ./*.in; do
     echo $file
-    ../build/ParaCL < $file
+    ../build/ParaCL $file
     echo
 done
 
@@ -21,6 +21,6 @@ echo -e "\e[1mTesting ill-formed paracl program\e[0m"
 echo
 for file in ill_formed/*.in; do
 echo $file
-    ./build/ParaCL < $file
+    ../build/ParaCL $file
     echo
 done
