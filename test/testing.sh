d8e2db39ef3ed878da5ca18efb8fd15a7b001869
Елисей <zhdanov.ea@phystech.edu>
2024-03-23 17:30:27 +0300

    e2e test check added

--- test/testing.sh
+++ test/testing.sh
@@ -7,7 +7,14 @@ echo -e "\e[1mTesting well-formed paracl program\e[0m"
 echo
 for file in well_formed/*.in; do
     echo $file
-    ../build/ParaCL $file
+    ../build/ParaCL $file < well_formed/input/${file:12:-3}.inp > well_formed/output/${file:12:-3}.outp
+    diff well_formed/correct/${file:12:-3}.corr well_formed/output/${file:12:-3}.outp
+    if [ $? = 0 ]
+    then
+        echo "Correct answer"
+    else
+        echo "Incorrect answer"
+    fi
     echo
 done
 
